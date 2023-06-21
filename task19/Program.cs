08e48017945f436e7cf8ff139639383eeb02225d
master <spikerus93@rambler.ru>
2023-06-21 23:10:35 +0300

    Выполнение домашнего задания по 3-ему Семинару

---
+++ task19/Program.cs
@@ -0,0 +1,33 @@
+﻿//  Напишите программу, которая принимает 
+// на вход пятизначное число и проверяет, является ли 
+// оно палиндромом.
+// 14212 -> нет
+// 23432 -> да
+// 12821 -> да
+
+Console.WriteLine("Введите Пятизначное Число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number >= 100000 || number > 99999)
+{
+    Console.WriteLine("Вы ввели не Пятизначное число");
+}
+else if (number >= 10000 && number <= 99999)
+{
+    int num1 = (number / 10000) % 10;
+    Console.Write($"Первая цифра: {num1} ");
+    int num5 = (number / 1) % 10;
+    Console.Write($"Пятая цифра: {num5} ");
+    int num2 = (number / 1000) % 10;
+    Console.Write($"Вторая цифра: {num2} ");
+    int num4 = (number / 10) % 10;
+    Console.Write($"Четвертая цифра: {num4} ");
+    if (num1 == num5 && num2 == num4)
+    {
+        Console.WriteLine($"Ваше Число {number} - это Палиндром");
+    }
+    else
+    {
+        Console.WriteLine($"Это Число - {number} не является Палиндромом");
+    }
+}
---
+++ task21/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек
+// и находит расстояние между ними в 3D пространстве.
+// A (3,6,8); B (2,1,-7), -> 15.84
+// A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите Координату Х Первой Точки: ");
+int x1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Координату Y Первой Точки: ");
+int y1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Координату Z Первой Точки: ");
+int z1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Координату Х Второй Точки: ");
+int x2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Координату Y Второй Точки: ");
+int y2 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите Координату Z Второй Точки: ");
+int z2 = Convert.ToInt32(Console.ReadLine());
+
+double  skobX = Math.Pow(x1 - x2, 2);
+double  skobY = Math.Pow(y1 - y2, 2);
+double  skobZ = Math.Pow(z1 - z2, 2);
+double result = Math.Sqrt(skobX+skobY+skobZ);
+
+Console.WriteLine($"{result:f2}");
